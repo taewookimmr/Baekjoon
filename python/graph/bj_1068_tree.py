6d71d845a6a6165596e8ba6c8fa52b2dbe204afb
taewookimmr <taewookimmr@gmail.com>
2019-10-04 17:04:50 +0900

    update

---
+++ python/graph/bj_1068_tree.py
@@ -0,0 +1,39 @@
+def solution():
+    import sys
+    sys.setrecursionlimit(100000)
+    
+    n = int(input())
+
+    parent = list(map(int, sys.stdin.readline().rstrip().split()))
+
+    graph = [[] for _ in range(n)]
+    for c, p in enumerate(parent):
+        if p != -1:
+            graph[p].append(c)
+    
+    target = int(input())
+    start = graph.pop(target)
+    graph.insert(target, [-1]) # [-1] mean it is deleted node 
+    
+    for g in graph:
+        if target in g:
+            g.remove(target)
+    
+    def recursive(e):
+        if len(e):
+            for target in e:
+                for g in graph:
+                    if target in g:
+                        g.remove(target)
+                temp = graph.pop(target)
+                graph.insert(target, [-1])
+                recursive(temp)
+    recursive(start)
+    count = 0
+    for g in graph:
+        if len(g) == 0:
+            count += 1
+    print(graph)            
+    print(count)
+
+solution()
---
+++ python/graph/bj_5567_wedding.py
@@ -0,0 +1,4 @@
+def solution():
+    pass
+
+solution()
