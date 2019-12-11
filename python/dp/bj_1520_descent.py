5dce6d4cfabe82e160111a732a27901ac2e1652e
taewookimmr <taewookimmr@gmail.com>
2019-12-12 00:15:00 +0900

    dynamic programming good

--- python/dp/bj_1520_descent.py
+++ python/dp/bj_1520_descent.py
@@ -6,24 +6,30 @@ def solution():
         
     r, c = getline()
     dp = [[0 for _ in range(c)] for _ in range(r)]
+    ck = [[0 for _ in range(c)] for _ in range(r)]
     dp[0][0]=1
     
     ground = []
     for _ in range(r):
         ground.append(getline())
         
-    count = [0]
     def recursive(rr, cc):
-        if rr == r-1 and cc == c-1:
-            count[-1] +=1 
-            return 
         for v in [1, -1]:
-            if rr+v < r and rr+v>=0 and ground[rr][cc] > ground[rr+v][cc]:
-                recursive(rr+v,cc)
-            if cc+v < c and cc+v>=0 and ground[rr][cc] > ground[rr][cc+v]:
-                recursive(rr,cc+v)
-    recursive(0,0)
-    print(count[-1])
+            if rr+v < r and rr+v>=0 and ground[rr][cc] < ground[rr+v][cc]:
+                if dp[rr+v][cc] or ck[rr+v][cc]:
+                    dp[rr][cc] += dp[rr+v][cc]
+                else:
+                    dp[rr][cc] +=recursive(rr+v,cc)
+            if cc+v < c and cc+v>=0 and ground[rr][cc] < ground[rr][cc+v]:
+                if dp[rr][cc+v] or ck[rr][cc+v]:
+                    dp[rr][cc] += dp[rr][cc+v]
+                else:
+                    dp[rr][cc] +=recursive(rr,cc+v)
+        ck[rr][cc]=1
+        return dp[rr][cc]
+    
+    recursive(r-1, c-1)
+    print(dp[-1][-1])
 
 
 solution()
