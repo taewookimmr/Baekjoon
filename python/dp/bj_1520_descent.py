152c34a8507cf8ea08d747d27288bcb9f5ae86d2
taewookimmr <taewookimmr@gmail.com>
2019-12-11 23:16:02 +0900

    ok

--- python/dp/bj_1520_descent.py
+++ python/dp/bj_1520_descent.py
@@ -1,4 +1,4 @@
-def solution_recursive():
+def solution():
     import sys
     sys.setrecursionlimit(10**6)
     def getline():
@@ -13,12 +13,11 @@ def solution_recursive():
         ground.append(getline())
         
     count = [0]
-
     def recursive(rr, cc):
         if rr == r-1 and cc == c-1:
             count[-1] +=1 
             return 
-        for v in [-1, 1]:
+        for v in [1, -1]:
             if rr+v < r and rr+v>=0 and ground[rr][cc] > ground[rr+v][cc]:
                 recursive(rr+v,cc)
             if cc+v < c and cc+v>=0 and ground[rr][cc] > ground[rr][cc+v]:
@@ -26,28 +25,6 @@ def solution_recursive():
     recursive(0,0)
     print(count[-1])
 
-def solution():
-    import sys
-    def getline():
-        return list(map(int, sys.stdin.readline().rstrip().split()))
-        
-    r, c = getline()
-    dp = [[0 for _ in range(c)] for _ in range(r)]
-    dp[0][0]=1
-    
-    ground = []
-    for _ in range(r):
-        ground.append(getline())
-        
-    for rr in range(r):
-        for cc in range(c):
-            for v in [-1, 1]:
-                if rr+v < r and rr+v>=0 and ground[rr][cc] > ground[rr+v][cc]:
-                    dp[rr+v][cc] += dp[rr][cc]
-                if cc+v < c and cc+v>=0 and ground[rr][cc] > ground[rr][cc+v]:
-                    dp[rr][cc+v] += dp[rr][cc]
 
-    print(dp[-1][-1])
-     
 solution()
 
