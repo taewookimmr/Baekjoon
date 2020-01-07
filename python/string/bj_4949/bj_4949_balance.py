7b15962a32c34c2761159cb349ae509a175175a3
taewookim <taewookim@taewookimui-MacBookAir.local>
2020-01-07 21:24:51 +0900

    docs:Edit bj_4949_balance.py

--- python/string/bj_4949/bj_4949_balance.py
+++ python/string/bj_4949/bj_4949_balance.py
@@ -1,4 +1,5 @@
 def solution():
+    ## 너무 생각없이 짰네,,,너무 길다..지저분스,,간단하게 만들자
     while True:
         mystr = [ch for ch in input().rstrip()]
         if len(mystr) == 1 and mystr[0]=='.' : break
@@ -8,21 +9,46 @@ def solution():
             if ch in ["(", "["]:
                 stack.append(ch)
             elif ch == ")":
-                if stack[-1] != "(":
-                    print("no")
-                    break
-                else :
-                    stack.pop()
-            elif ch == "]":
-                if stack[-1] != "[":
-                    print("no")
-                    break
+                if len(stack):
+                    if stack[-1] != "(":
+                        print("no")
+                        break
+                    else :
+                        stack.pop()
                 else:
-                    stack.pop()
+                    print("no")
+                    break
+            elif ch == "]":
+                if len(stack):
+                    if stack[-1] != "[":
+                        print("no")
+                        break
+                    else:
+                        stack.pop()
+                else:
+                    print("no")
+                    break
         else:
             if len(stack):
                 print("no")
             else:
                 print("yes") 
+
+def solution_other():
+    check ='([])'
+    while True:
+        src = input().rstrip()
+        if src == ".\n": break
+        temp = []
+        for ch in src:
+            if ch in check:
+                if ch == ')' and len(temp) > 0 and temp[-1] == "(":
+                    temp.pop()
+                elif ch == ']' and len(temp) > 0 and temp[-1] == "[":
+                    temp.pop()
+                else:
+                    temp.append(ch)
+        print('yes' if no temp else 'no')
+  
 solution()
 
