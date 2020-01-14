451b310321218bdcec5eb0b0197a90d12f3d3806
taewookimmr <taewookimmr@gmail.com>
2020-01-15 07:45:15 +0900

    Add bj1371 java version

--- java/string/bj_1371/bj_1371_frequentchar/src/app/App.java
+++ java/string/bj_1371/bj_1371_frequentchar/src/app/App.java
@@ -1,7 +1,28 @@
 package app;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        char[] arr = new char[26];
+        int max = 0;
+        while(sc.hasNextLine()){
+            char[] input = sc.nextLine().toCharArray();
+            System.out.println(input);
+            for(int i = 0; i<input.length; i++){
+                if(input[i] != ' '){
+                    max = Math.max(max, ++arr[input[i]-'a']);
+                }
+            }
+        }
+        for (int i =0; i < arr.length;i++){
+            if(arr[i]==max){
+                sb.append((char)('a' + i));
+            }
+        }
+        sb.append(System.lineSeparator());
+        System.out.println(sb.toString());
     }
 }
