3e87f69692cb1830804b4cc3d16a70bb766534d9
taewookimmr <taewookimmr@gmail.com>
2020-01-16 19:18:29 +0900

    docs:Change java file name

---
+++ java/string/bj_2998/src/app/Bj2998.java
@@ -0,0 +1,13 @@
+package app;
+
+import java.util.Scanner;
+
+public class Bj2998 {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        String numstr = sc.nextLine();
+        int n = Integer.parseInt(numstr);
+        System.out.println(n);
+        sc.close();
+    }
+}
