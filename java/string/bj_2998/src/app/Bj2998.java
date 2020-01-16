e7bd4a4d42bd6622c20d43953c65886c9387645e
taewookimmr <taewookimmr@gmail.com>
2020-01-16 19:42:50 +0900

    docs:bj2998 problem solved

--- java/string/bj_2998/src/app/Bj2998.java
+++ java/string/bj_2998/src/app/Bj2998.java
@@ -6,8 +6,25 @@ public class Bj2998 {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         String numstr = sc.nextLine();
-        int n = Integer.parseInt(numstr);
-        System.out.println(n);
+        int strlen = numstr.length();
+        switch(strlen%3){
+            case 1:
+                numstr = "0".concat(numstr);
+            case 2:
+                numstr = "0".concat(numstr);
+                break;
+        }
+
+        String answer = "";
+        for(int i = 0; i < numstr.length(); i+=3){
+            int digit = 4*(numstr.charAt(i)-'0');
+            digit += 2*(numstr.charAt(i+1)-'0');
+            digit += 1*(numstr.charAt(i+2)-'0');
+            
+            answer += digit;
+        }
+
+        System.out.println(answer);
         sc.close();
     }
 }
