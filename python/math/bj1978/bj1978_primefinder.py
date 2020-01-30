718f85ed956e5130e13af87d94b90c49041f9b6d
taewookimmr <taewookimmr@gmail.com>
2020-01-30 23:07:53 +0900

    docs:Add bj1987 Problem to Python dir

--- python/math/bj1978/bj1978_primefinder.py
+++ python/math/bj1978/bj1978_primefinder.py
@@ -1,5 +1,10 @@
 def solution():
-    pass
-
+    def isPrime(n): 
+        import math
+        for e in range(2, int(math.sqrt(n))+1):
+            if n%e == 0 : return False
+        return True
+    _ = input()
+    print(sum([1 if isPrime(e) else 0 for e in list(map(int, input().split()))]))    
 
 solution()
